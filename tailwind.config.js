60b03ce716ddf6b8df6b4c228fc6b61859595ae7
SHAWA ZAHMA AZ-ZAHARA <222112364@stis.sc.id>
2024-06-07 11:30:11 +0700

    add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ export default {
     fontFamily: {
       'jakarta': ['Plus Jakarta Sans', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        neutral1: '#2D2926',
+        neutral2: '#63666A',
+        neutral3: '#75787B',
+        neutral4: '#BBBCBC',
+        neutral5: 'rgba(217, 217, 214, 0.6)',
+      },
+    },
   },
   plugins: [],
 }

447778223cc27c43a6eb2f0bb113a80adfd9661b
Rifky Maulana Putra <222112322@stis.ac.id>
2024-06-09 00:06:22 +0700

    update hero dan tim pengarah

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-neutral1 text-white py-10 absolute w-full bottom-0">
+  <footer class="bg-neutral-1 text-white py-10 absolute w-full bottom-0">
     <div class="container mx-auto flex flex-wrap justify-between">
       <div class="w-full md:w-1/4 mb-6 md:mb-0">
         <img src="../assets/logo2.svg" alt="Logo" class="md:w-1/2 ml-16 mb-4" />
@@ -7,7 +7,7 @@
           Open Government Indonesia (OGI) merupakan sebuah gerakan keterbukaan
           dan partisipasi melalui kolaborasi inovatif.
         </p>
-        <div class="flex  ml-16 mt-16">
+        <div class="flex ml-16 mt-16">
           <a href="#" class="mr-4">
             <img src="../assets/Facebook.svg" alt="Facebook" class="h-9" />
           </a>
@@ -15,11 +15,7 @@
             <img src="../assets/Twitter.svg" alt="Twitter" class="h-9" />
           </a>
           <a href="#" class="mr-4">
-            <img
-              src="../assets/Instagram.svg"
-              alt="Instagram"
-              class="h-9"
-            />
+            <img src="../assets/Instagram.svg" alt="Instagram" class="h-9" />
           </a>
           <a href="#">
             <img src="../assets/Youtube.svg" alt="YouTube" class="h-9" />
@@ -29,21 +25,17 @@
       <div class="w-full md:w-1/4 mb-6 md:mb-0 pl-11">
         <h2 class="font-semibold mb-4">Kontak Info</h2>
         <h2 class="font-semibold">Alamat</h2>
-        <p class="text-neutral5 mb-4">
+        <p class="text-neutral-5 mb-4">
           Jl. Taman Suropati No.2 Menteng, Jakarta Pusat 10310
         </p>
         <h2 class="font-semibold">Telepon</h2>
-        <p class="text-neutral5 mb-4">
-          (62-21) 3148551 ext . 3504, 3503
-        </p>
+        <p class="text-neutral-5 mb-4">(62-21) 3148551 ext . 3504, 3503</p>
         <h2 class="font-semibold">Email</h2>
-        <p class="text-neutral5">
-          sekretariat.ogi@bappenas.go.id
-        </p>
+        <p class="text-neutral-5">sekretariat.ogi@bappenas.go.id</p>
       </div>
-      <div class="w-full md:w-1/4 mb-6 md:mb-0 ">
+      <div class="w-full md:w-1/4 mb-6 md:mb-0">
         <h2 class="font-semibold mb-4 ml-11">Eksplorasi</h2>
-        <ul class="text-neutral5 ml-11">
+        <ul class="text-neutral-5 ml-11">
           <li class="mb-2">
             <a href="#" class="hover:text-white">Tentang Kami</a>
           </li>
@@ -60,8 +52,8 @@
         </ul>
       </div>
       <div class="w-full md:w-1/4">
-        <h2 class="font-semibold mb-4 ">Tautan Lainnya</h2>
-        <ul class="text-neutral5">
+        <h2 class="font-semibold mb-4">Tautan Lainnya</h2>
+        <ul class="text-neutral-5">
           <li class="mb-2">
             <a href="#" class="hover:text-white">BAPPENAS</a>
           </li>
@@ -82,8 +74,8 @@
         </ul>
       </div>
     </div>
-    <div class="border-t border-neutral3 mt-6 pt-4 mx-28">
-      <p class="text-center text-neutral5">
+    <div class="border-t border-neutral-3 mt-6 pt-4 mx-28">
+      <p class="text-center text-neutral-5">
         Hak Cipta © 2022 Open Government Indonesia
       </p>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,112 @@
 <template>
   <div id="app">
     <Header />
-    <div class="container mx-auto text-center">
-      <div class="h-[calc(50vh)]"></div>
-      <h3 class="text-sm mb-8 text-gray-400">
+    <!-- Hero -->
+    <div
+      id="hero"
+      class="container mx-auto max-w-5xl text-center relative z-1000"
+    >
+      <div class="h-[calc(30vh)]"></div>
+      <h3 class="text-sm mb-6 text-neutral-2">
         Mewujudkan Pemerintahan Terbuka dan Inovatif Bersama Rakyat
       </h3>
-      <h1 class="text-primary-2 font-bold text-3xl">
+      <h1 class="text-primary-2 font-bold text-7xl mb-8">
         Open Government Indonesia
       </h1>
-      <h3>
+      <h3 class="font-bold text-xl">
         Bersama Open Government Indonesia (OGI), kami berkomitmen menciptakan
         solusi inovatif melalui kolaborasi antara pemerintah, masyarakat sipil,
         akademisi, dan mitra pembangunan untuk masa depan yang lebih transparan
         dan partisipatif.
       </h3>
-      <button>
-        Cari Tahu Lebih Lanjut
-        <i data-feather="arrow-right" class="bg-white inline"></i>
-      </button>
+      <a href="#" class="cursor-pointer z-1000">
+        <button
+          class="bg-primary-1 hover:bg-primary-2 text-white font-semibold py-2 px-6 rounded-full shadow-sm transition duration-300 shadow-primary-1 group mt-4 cursor-pointer relative"
+        >
+          Cari Tahu Lebih Lanjut
+          <i
+            data-feather="arrow-right"
+            class="bg-primary-1 text-white inline transition-all group-hover:bg-primary-2"
+          ></i>
+        </button>
+      </a>
     </div>
+
+    <div class="z-0">
+      <div
+        class="flex justify-start items-start absolute top-[-5.5rem] -left-40 w-2/3"
+      >
+        <img src="../assets/grafik-home-1.png" alt="" class="w-1/2" />
+      </div>
+      <div class="flex flex-row justify-end absolute top-36 -right-56 w-3/4">
+        <img src="../assets/grafik-home-2.png" alt="" class="w-1/2" />
+      </div>
+    </div>
+
+    <!-- RAN Card -->
+    <div class="bg-primary-5/10 py-12 mt-36 ml-4 mr-4 rounded-3xl">
+      <div class="container mx-auto text-center">
+        <h2 class="text-4xl font-bold mb-4 text-primary-1">
+          <span class="text-neutral-1">Apa itu</span> Rencana Aksi Nasional
+          (RAN)?
+        </h2>
+        <p class="text-lg mb-6 max-w-3xl mx-auto text-neutral-1">
+          Langkah nyata menuju transparansi dan partisipasi publik.
+          Bersama-sama, kita wujudkan pemerintahan yang inovatif, akuntabel, dan
+          inklusif!
+        </p>
+        <a href="#" class="cursor-pointer">
+          <button
+            class="bg-primary-1 hover:bg-primary-2 text-white font-semibold py-2 px-6 rounded-full shadow-sm transition duration-300 shadow-primary-1 group mt-4 cursor-pointer"
+          >
+            Lihat Rencana Aksi Nasional
+          </button>
+        </a>
+      </div>
+    </div>
+
+    <!-- Tim Pengarah -->
+    <div class="container mx-auto py-12 text-center max-w-5xl">
+      <h2 class="text-4xl font-bold mb-4">
+        Siapa <span class="text-red-500">tim pengarah</span> OGI? 👀
+      </h2>
+      <p class="text-lg mb-8">
+        Tim Pengarah OGI memberikan arahan strategis untuk mendorong
+        pemerintahan yang transparan dan partisipatif melalui kolaborasi antara
+        pemerintah, masyarakat sipil, akademisi, dan mitra pembangunan.
+      </p>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <LogoCard description="Kantor Staf Wakil Presiden" logoSrc="logo-ksp.png" />
+        <LogoCard description="Kementrian Luar Negeri" logoSrc="logo-kemlu.png" />
+        <LogoCard description="Badan Perencanaan Pembagunan Nasional" logoSrc="logo-bappenas.png" />
+        <LogoCard description="Kementrian Dalam Negeri" logoSrc="logo-kemdagri.png"/>
+        <LogoCard description="Kementrian Komunikasi dan Informatika RI" logoSrc="logo-kominfo.png"/>
+        <LogoCard description="International NGO Forum on Indonesian Development" logoSrc="logo-imfid.png"/>
+        <LogoCard description="Komisi Informasi Pusat" logoSrc="logo-KIP.png"/>
+        <LogoCard description="Kementrian PANRB" logoSrc="logo-panrb.png"/>
+      </div>
+    </div>
+
     <div class="h-[calc(100vh*2)]"></div>
-    <Footer />
   </div>
 </template>
-    
-  <script>
+
+<script>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
+import LogoCard from "../components/LogoCard.vue";
 
 export default {
   components: {
     Header,
     Footer,
+    LogoCard,
+  },
+
+  data() {
+    return {
+      logoSrc: "logo-ksp.png",
+    };
   },
 };
 </script>

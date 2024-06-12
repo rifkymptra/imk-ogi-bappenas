e2e80670aaaa1341753be92ca426835f28c9ef33
Rifky Maulana Putra <222112322@stis.ac.id>
2024-06-12 14:29:35 +0700

    update komitmen

--- src/components/TitleSection.vue
+++ src/components/TitleSection.vue
@@ -1,48 +1,54 @@
 <template>
-    <div class="container mx-auto flex flex-col items-center">
-      <div class="h-[calc(20vh)]"></div>
-      <a :href="link" class="font-bold flex items-center text-primary-1 hover:text-red-500 mb-1 group">
-        <i data-feather="arrow-left" class="mr-1 transition-transform transform group-hover:-translate-x-1"></i>
-        {{ linkText }}
-      </a>
-      <h1 class="text-[4.5rem] font-bold my-5">
-        {{ heading }} <span class="text-primary-1">{{ subheading }}</span>
-      </h1>
-      <div class="h-[calc(12vh)]"></div>
-    </div>
-  </template>
+  <div class="container mx-auto flex flex-col items-center">
+    <div class="h-[calc(20vh)]"></div>
+    <a
+      :href="link"
+      class="font-bold flex items-center text-primary-1 hover:text-red-500 mb-1 group"
+    >
+      <i
+        data-feather="arrow-left"
+        class="mr-1 transition-transform transform group-hover:-translate-x-1"
+      ></i>
+      {{ linkText }}
+    </a>
+    <h1 class="text-[4.5rem] font-bold my-5">
+      {{ heading }} <span class="text-primary-1">{{ subheading }}</span>
+    </h1>
+    <div class="h-[calc(12vh)]"></div>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: "TitleSection",
-    props: {
-      link: {
-        type: String,
-        required: true
-      },
-      linkText: {
-        type: String,
-        required: true
-      },
-      heading: {
-        type: String,
-        required: true
-      },
-      subheading: {
-        type: String,
-        required: true
-      }
+export default {
+  name: "TitleSection",
+  props: {
+    link: {
+      type: String,
+      required: true,
     },
-    mounted() {
-      // Initialize Feather icons
-      this.$nextTick(() => {
-        feather.replace()
-      });
-    }
-  };
-  </script>
+    linkText: {
+      type: String,
+      required: true,
+    },
+    heading: {
+      type: String,
+      required: true,
+    },
+    subheading: {
+      type: String,
+      required: true,
+    },
+  },
+  mounted() {
+    // Initialize Feather icons
+    this.$nextTick(() => {
+      feather.replace();
+    });
+  },
+};
+</script>
   
   <style scoped>
-  /* Add any component-specific styles here */
-  </style>
+/* Add any component-specific styles here */
+</style>
   
--- src/views/RAN/Komitmen.vue
+++ src/views/RAN/Komitmen.vue
@@ -1,16 +1,134 @@
 <template>
-  <div class="container mx-auto mt-24 py-12 text-center">
-    <h1 class="text-[3.5rem] font-bold mb-6">Rencana Aksi Nasional</h1>
-    <p class="mb-8 max-w-[59rem] mx-auto font-inter text-xl">
-      Rencana Aksi Nasional (RAN) merupakan sebuah dokumen yang menjadi inti
-      pelaksanaan praktik keterbukaan pemerintah oleh Open Government Indonesia
-      (OGI). RAN berisikan komitmen-komitmen hasil dari ko-kreasi antara K/L
-      bersama dengan Organisasi Masyarakat Sipil (OMS). Setiap komitmen
-      berisikan ukuran keberhasilan yang rinci untuk memastikan terlaksananya
-      komitmen tersebut. Setiap komitmen juga mencerminkan nilai-nilai OGI yakni
-      <span class="font-bold"
-        >Transparansi, Partisipasi, Inovasi, Akuntabilitas, dan Inklusif.</span
-      >
-    </p>
+  <Header />
+  <div class="container mx-auto text-center h-[1000vh]">
+    <TitleSection
+      link="/ran"
+      linkText="RENCANA AKSI NASIONAL"
+      heading="Komitmen"
+      subheading="RAN OGI VII"
+    />
+    <div class="mb-8 max-w-[59rem] mx-auto font-inter text-xl">
+      <p>
+        Sebagai upaya implementasi keterbukaan pemerintah, Sekretariat Nasional
+        Open Government Indonesia berhasil menyusun Rencana Aksi Nasional Open
+        Goverment Indonesia (RAN OGI) untuk periode tahun 2023-2024.
+      </p>
+      <p class="mt-4">
+        Berisi 15 Komitmen keterbukaan pemerintah yang multisektoral, perumusan
+        RAN OGI diselenggarakan bersama-sama melalui mekanisme ko-kreasi antara
+        Kementrian/Lembaga dan Organisasi Masyarakat Sipil (OMS).
+      </p>
+    </div>
+
+    <KomitmenList
+      title="Mendorong transparansi dan partisipasi dalam pengadaan barang/jasa pemerintah"
+      textToAdd="mweehehehehe"
+      number="1"
+      :link="'/ran/komitmen/1'"
+    />
+    <KomitmenList
+      title="Optimalisasi implementasi keterbukaan data benefical ownership"
+      textToAdd="mweehehehehe"
+      number="2"
+    />
+    <KomitmenList
+      title="Perlindungan hukum dalam perkara lingkungan hidup"
+      textToAdd="mweehehehehe"
+      number="3"
+    />
+
+    <KomitmenList
+      title="Mengawal perumusan peraturan pelaksana undang-undang no. 27 tahun 2022 tentang pelindungan data pribadi (UU PDP)"
+      textToAdd="mweehehehehe"
+      number="4"
+    />
+
+    <KomitmenList
+      title="Penyusunan mekanisme penanganan pemilu dari disinformasi"
+      textToAdd="mweehehehehe"
+      number="5"
+    />
+
+    <KomitmenList
+      title="Keterbukaan tata kelola data pemilihan umum"
+      textToAdd="mweehehehehe"
+      number="6"
+    />
+
+    <KomitmenList
+      title="Penyediaan pedoman aksesibilitas digital layanan komunikasi dan informasi publik yang inklusif bagi penyandang disabilitas"
+      textToAdd="mweehehehehe"
+      number="7"
+    />
+
+    <KomitmenList
+      title="Akuntabilitas sosial keterbukaan anggaran desa"
+      textToAdd="mweehehehehe"
+      number="8"
+    />
+
+    <KomitmenList
+      title="Akuntabilitas sosial pelayanan publik dan tata kelola pemerintahan desa"
+      textToAdd="mweehehehehe"
+      number="9"
+    />
+
+    <KomitmenList
+      title="Penyusunan Rancangan Undang-Undang Komisi Kebenaran dan Rekonsiliasi (RUU KKR) untuk memenuhi hak atas kebenaran"
+      textToAdd="mweehehehehe"
+      number="10"
+    />
+
+    <KomitmenList
+      title="Aksesibilitas dan akuntabilitas proses peradilan di Indonesia"
+      textToAdd="mweehehehehe"
+      number="11"
+    />
+
+    <KomitmenList
+      title="Perluasan pemberian bantuan hukum yang berkualitas dan aksesibel di Indonesia"
+      textToAdd="mweehehehehe"
+      number="12"
+    />
+
+    <KomitmenList
+      title="Penguatan bantuan hukum bagi kelompok rentan di Indonesia"
+      textToAdd="mweehehehehe"
+      number="13"
+    />
+
+    <KomitmenList
+      title="Penguatan mekanisme pemulihan dalam Undang-Undang no. 12 Tahun 2022 tentang Tindak Pidana Kekerasan Seksual (UU TPKS)"
+      textToAdd="mweehehehehe"
+      number="14"
+    />
+
+    <KomitmenList
+      title="Perlindungan hukum dalam perkara lingkungan hidup"
+      textToAdd="mweehehehehe"
+      number="15"
+    />
   </div>
+
+  <Footer />
+
+  <ScrollButton />
 </template>
+
+<script>
+import Header from "../../components/Header.vue";
+import Footer from "../../components/Footer.vue";
+import ScrollButton from "../../components/ScrollButton.vue";
+import TitleSection from "../../components/TitleSection.vue";
+import KomitmenList from "../../components/RAN/KomitmenList.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+    ScrollButton,
+    TitleSection,
+    KomitmenList,
+  },
+};
+</script>

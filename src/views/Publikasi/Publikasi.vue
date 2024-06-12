5c673812acdfe639ce7fc063f9b4b854adb2adb3
Aditya <adit.widi@gmail.com>
2024-06-12 11:09:35 +0700

    update publikasi views

--- src/views/Publikasi/Publikasi.vue
+++ src/views/Publikasi/Publikasi.vue
@@ -1,60 +1,64 @@
 <template>
-    <div id = "app">
-     <Header />
-     <div class="container mx-auto text-center">
-         <div class="h-[calc(20vh)]"></div>
-         <h1 class="text-[3.5rem] font-bold my-5">
-             Publikasi <span class="text-primary-1">OGI</span></h1>
-         
-       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 my-5">
-         <Card
-         image="require('@/assets/foto-1.png')" 
-         title="Tentang Kami" 
-         description="Pelajari Open Government Indonesia secara lebih detail."
-         linkText="Baca lebih lanjut"
-         link="#"
-       />
-         <Card 
-         image="../assets/foto-2.png" 
-         title="Sekretariat Kami" 
-         description="Kenali orang-orang yang ada di dalam sekretariat kami."
-         linkText="Kenali sekretariat kami"
-         link="#"
-       />
-       <Card 
-         image="../assets/foto-3.png" 
-         title="Kegiatan Kami" 
-         description="Kepoin kegiatan-kegiatan yang sudah kami lakukan di OGI."
-         linkText="Lihat kegiatan"
-         link="#"
-       />
-       <Card 
-         image="../assets/foto-4.png" 
-         title="Karir OGI" 
-         description="Gabung bersama tim OGI."
-         linkText="Lihat lowongan"
-         link="#"
-       />
-       </div>
-         <div class="h-[calc(12vh)]"></div>
-     </div>
-    
-     <Footer />
- 
+    <div id="app">
+      <Header />
+      <div class="container mx-auto flex flex-col items-center">
+        <div class="h-[calc(20vh)]"></div>
+        <h1 class="text-4xl font-bold my-5">
+          Publikasi <span class="text-primary-1">OGI</span>
+        </h1>
+        
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-5 my-5">
+          <Card
+            image="/src/assets/news.jpg"
+            title="OGI News"
+            description="Baca berita terkini terkait OGI."
+            linkText="Baca lebih lanjut"
+            link="#"
+          />
+          <Card
+            image="/src/assets/infografis.jpg"
+            title="Infografis"
+            description="Hasil pengolahan dan analisis oleh tim OGI."
+            linkText="Lihat infografis"
+            link="#"
+          />
+          <Card
+            image="/src/assets/laporan kajian.jpg"
+            title="Laporan dan Kajian"
+            description="Laporan pelaksanaan dari kegiatan yang telah dilakukan."
+            linkText="Unduh Berkas"
+            link="#"
+          />
+          <Card
+            image="/src/assets/IRM.jpg"
+            title="IRM"
+            description="Independent Reporting Mechanism"
+            linkText="Lihat lowongan"
+            link="#"
+          />
+        </div>
+        <div class="h-[calc(12vh)]"></div>
+      </div>
+      <Footer />
     </div>
- </template>
- 
- <script>
- import Header from "../../components/Header.vue";
- import Footer from "../../components/Footer.vue";
- import Card from "../../components/Card.vue";
- export default {
-     name : "Publikasi",
-   components: {
-     Header,
-     Footer,
-     Card,
-   },
-   
- };  
- </script>
+  </template>
+  
+  <script>
+  import Header from "../../components/Header.vue";
+  import Footer from "../../components/Footer.vue";
+  import Card from "../../components/Card.vue";
+  
+  export default {
+    name: "Publikasi",
+    components: {
+      Header,
+      Footer,
+      Card,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any additional styles if necessary */
+  </style>
+  

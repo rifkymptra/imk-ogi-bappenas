b93c2826996434eaf51e2f18afd96ad578fba592
Aditya <adit.widi@gmail.com>
2024-06-12 12:21:53 +0700

    add news page and component title

--- src/main.js
+++ src/main.js
@@ -9,12 +9,14 @@ import App from './App.vue';
 import Pelajari from './views/Pelajari/Pelajari.vue';
 import Karir from './views/Pelajari/Karir.vue';
 import Publikasi from './views/Publikasi/Publikasi.vue';
+import News from './views/Publikasi/News.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/pelajari', component: Pelajari }, 
   { path: '/karir', component: Karir },
-  { path: '/publikasi', component: Publikasi} 
+  { path: '/publikasi', component: Publikasi},
+  { path: '/publikasi/ogi-news', component: News}
 ];
 
 const router = createRouter({

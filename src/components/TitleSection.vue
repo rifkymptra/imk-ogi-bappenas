c5ed452e72b634b61b6b04b768072a392d1299a3
Aditya <adit.widi@gmail.com>
2024-06-12 12:37:30 +0700

    update title section

--- src/components/TitleSection.vue
+++ src/components/TitleSection.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container mx-auto flex flex-col items-center">
-      <div class="h-[calc(20vh)]"></div>
+    <div class="container mx-auto flex flex-col items-center mb-5">
+      <div class="h-[calc(10vh)]"></div>
       <a :href="link" class="font-bold flex items-center text-primary-1 hover:text-red-500 mb-1 group">
         <i data-feather="arrow-left" class="mr-1 transition-transform transform group-hover:-translate-x-1"></i>
         {{ linkText }}
@@ -8,7 +8,6 @@
       <h1 class="text-[4.5rem] font-bold my-5">
         {{ heading }} <span class="text-primary-1">{{ subheading }}</span>
       </h1>
-      <div class="h-[calc(12vh)]"></div>
     </div>
   </template>
   
@@ -42,7 +41,5 @@
   };
   </script>
   
-  <style scoped>
-  /* Add any component-specific styles here */
-  </style>
+
   
--- src/views/Publikasi/News.vue
+++ src/views/Publikasi/News.vue
@@ -2,11 +2,31 @@
     <div id="app">
       <Header />
       <TitleSection 
-      link="/publikasi" 
-      linkText="PUBLIKASI OGI" 
-      heading="OGI"
-      subheading="News" 
+        link="/publikasi" 
+        linkText="PUBLIKASI OGI" 
+        heading="OGI"
+        subheading="News" 
       />
+      <div class="container mx-auto flex flex-col items-center mt-5">
+        <div class="w-full md:w-1/2 relative">
+          <input
+            type="text"
+            placeholder="Telusuri"
+            class="w-full py-2 px-4 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-500"
+          />
+          <button
+            class="absolute right-0 top-0 mt-2 mr-4 text-gray-400 hover:text-gray-600 focus:outline-none"
+            @click="clearSearch"
+          >
+            <i data-feather="x"></i>
+          </button>
+          <button
+            class="absolute right-0 top-0 mt-2 mr-10 text-gray-400 hover:text-gray-600 focus:outline-none"
+          >
+            <i data-feather="search"></i>
+          </button>
+        </div>
+      </div>
       <Footer />
     </div>
   </template>
@@ -28,6 +48,11 @@
       this.$nextTick(() => {
         feather.replace()
       })
+    },
+    methods: {
+      clearSearch() {
+        // Add your search clearing logic here
+      }
     }
   };
   </script>

72989be9c826a768c0dab51229aa84788633b555
Rifky Maulana Putra <222112322@stis.ac.id>
2024-06-24 03:24:24 +0700

    fix Perjalanan

--- src/views/Pelajari/Perjalanan.vue
+++ src/views/Pelajari/Perjalanan.vue
@@ -61,16 +61,16 @@
             </h2>
             <div class="flex space-x-4 mt-2 ml-5 font-inter text-neutral-2">
               <LogoCard
-                LogoSrc="/src/assets/bangai.png"
+                LogoSrc="bangai.png"
                 alt="Kab. Banggai"
                 label="Kab. Bangai"
               />
               <LogoCard
-                LogoSrc="/src/assets/semarang.png"
+                LogoSrc="/assets/semarang.png"
                 alt="Kota Semarang"
                 label="Kota Semarang"
               />
-              <LogoCard LogoSrc="/src/assets/ntb.png" alt="NTB" label="NTB" />
+              <LogoCard LogoSrc="@/assets/ntb.png" alt="NTB" label="NTB" />
               <LogoCard
                 LogoSrc="/src/assets/sumbawa.png"
                 alt="Sumbawa Barat"
